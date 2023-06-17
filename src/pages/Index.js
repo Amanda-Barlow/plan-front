5c7a2872458c2cbf6309c8d49759c126f4a9d835
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-06-17 00:19:52 -0800

    done

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,39 +1,41 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Index(props) {
-  // state to hold formData
   const [newForm, setNewForm] = useState({
     name: "",
     goal: "",
-    skills: "", 
+    skills: "",
+    frequency: "",
+    coping: "",
+    contacts: "",
   });
 
-  // handleChange function for form
   const handleChange = (event) => {
-    setNewForm({ ...newForm, [event.target.name]: event.target.value }); 
+    setNewForm({ ...newForm, [event.target.name]: event.target.value });
   };
 
-  // handle submit function for form
   const handleSubmit = (event) => {
     event.preventDefault();
-    props.createPlan(newForm); 
+    props.createPlan(newForm);
     setNewForm({
       name: "",
       goal: "",
-      skills: ""
+      skills: "",
+      frequency: "",
+      coping: "",
+      contacts: "",
     });
   };
 
-  // loaded function
   const loaded = () => {
-    return props.plans.map((plan) => ( 
+    return props.plans.map((plan) => (
       <div key={plan._id} className="plan">
         <Link to={`/plan/${plan._id}`}>
-          <h2>{plan.name}</h2> 
-        </Link>;
-        {/* <h2>{plan.skills}</h2> 
-        <h3>{plan.goal}</h3>  */}
+          <h2>{plan.name}</h2>
+        </Link>
+        <h2>{plan.skills}</h2>
+        <h3>{plan.goal}</h3>
       </div>
     ));
   };
@@ -41,6 +43,7 @@ function Index(props) {
   const loading = () => {
     return <h2>Let's Make Your Plan</h2>;
   };
+
   return (
     <section>
       <form onSubmit={handleSubmit}>
@@ -60,37 +63,37 @@ function Index(props) {
         />
         <input
           type="text"
-          value={newForm.skills} 
-          name="skills" 
-          placeholder="current skills" 
+          value={newForm.skills}
+          name="skills"
+          placeholder="current skills"
           onChange={handleChange}
         />
         <input
           type="text"
-          value={newForm.frequency} 
-          name="frequency" 
-          placeholder="frequency of meeting" 
+          value={newForm.frequency}
+          name="frequency"
+          placeholder="frequency of meeting"
           onChange={handleChange}
         />
         <input
           type="text"
-          value={newForm.coping} 
-          name="coping" 
-          placeholder="coping skills" 
+          value={newForm.coping}
+          name="coping"
+          placeholder="coping skills"
           onChange={handleChange}
         />
         <input
           type="text"
-          value={newForm.contacts} 
-          name="contacts" 
-          placeholder="supportive contact" 
+          value={newForm.contacts}
+          name="contacts"
+          placeholder="supportive contact"
           onChange={handleChange}
         />
- <input type="submit" value="Create Plan" /> 
- </form>
-{props.plans ? loaded() : loading()} 
-</section>
-  )
-};
+        <input type="submit" value="Create Plan" />
+      </form>
+      {props.plans ? loaded() : loading()}
+    </section>
+  );
+}
 
 export default Index;
